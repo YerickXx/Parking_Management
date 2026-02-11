41b5bee67f55ea3a55a9b50d33ab0e39360424e2
Yerick Abarca <yerickabarca@gmail.com>
2026-02-11 11:56:04 -0600

    Se agrega a la opcion 1 el sistema de cobro en base a duracion y se arregla una validacion

--- OneDrive/Documentos/AbarcaYerick_Proyecto1_IC2026/AbarcaYerick_Proyecto1_IC2026/src/Objetos/ServicioParqueo.java
+++ OneDrive/Documentos/AbarcaYerick_Proyecto1_IC2026/AbarcaYerick_Proyecto1_IC2026/src/Objetos/ServicioParqueo.java
@@ -10,21 +10,22 @@ package Objetos;
 /*
  * @author yeric
  */
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class ServicioParqueo { // Objeto servicio de parqueo
-    private double Day_Price;
-    private double Hour_Price;
-    
-    
+
+    private int Day_Price;
+    private int Hour_Price;
+
     public ServicioParqueo() // constructor vacio del objeto Servicio parqueo
     {
-        this.Day_Price = 0.0;
-        this.Hour_Price = 0.0;
+        this.Day_Price = 0;
+        this.Hour_Price = 0;
     }
-    
+
     // Constructor sobrecargado de la clase
-    public ServicioParqueo(double pDay_Price, double pHour_Price)
-    {
+    public ServicioParqueo(int pDay_Price, int pHour_Price) {
         this.Day_Price = pDay_Price;
         this.Hour_Price = pHour_Price;
     }
@@ -33,7 +34,7 @@ public class ServicioParqueo { // Objeto servicio de parqueo
         return Day_Price;
     }
 
-    public void setDay_Price(double Day_Price) {
+    public void setDay_Price(int Day_Price) {
         this.Day_Price = Day_Price;
     }
 
@@ -41,52 +42,65 @@ public class ServicioParqueo { // Objeto servicio de parqueo
         return Hour_Price;
     }
 
-    public void setHour_Price(double Hour_Price) {
+    public void setHour_Price(int Hour_Price) {
         this.Hour_Price = Hour_Price;
     }
-    
-    
-    public void Calculo_Pago_dia(String Type)
-    {
-        switch(Type)
-        {
-            case "Automovil" ->
-            {
-             ServicioParqueo.this.setDay_Price(5000.00);
+
+    public void Calculo_Pago_dia(String Type) {
+        switch (Type) {
+            case "Automovil" -> {
+                ServicioParqueo.this.setDay_Price(5000);
             }
-            case "Motocicleta" ->
-            {
-                ServicioParqueo.this.setDay_Price(3000.00);
+            case "Motocicleta" -> {
+                ServicioParqueo.this.setDay_Price(3000);
                 break;
             }
-            case "Camion" ->
-            {
-                ServicioParqueo.this.setDay_Price(5000.00);
+            case "Camion" -> {
+                ServicioParqueo.this.setDay_Price(5000);
                 break;
             }
         }
     }
-    
-    public void Calculo_Pago_Hora (String Type)
-    {
-        switch(Type)
-        {
-            case "Automovil" ->
-            {
-             ServicioParqueo.this.setHour_Price(600.00);
+
+    public void Calculo_Pago_Hora(String Type) {
+        switch (Type) {
+            case "Automovil" -> {
+                ServicioParqueo.this.setHour_Price(600);
             }
-            case "Motocicleta" ->
-            {
-                ServicioParqueo.this.setDay_Price(500.00);
+            case "Motocicleta" -> {
+                ServicioParqueo.this.setHour_Price(500);
                 break;
             }
-            case "Camion" ->
-            {
-                ServicioParqueo.this.setDay_Price(600.00);
+            case "Camion" -> {
+                ServicioParqueo.this.setHour_Price(600);
                 break;
             }
         }
     }
-    
-    
+
+    public String Opciones_Tiempo(Vehiculo vehiculo) 
+    {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            
+            System.out.print("Cuantas horas o dias estara el vehiculo en el recinto(ej: 2): ");
+            String ops = scanner.next();
+             int num = Integer.parseInt(ops);
+             
+                    if (vehiculo.getService().equalsIgnoreCase("por hora")) {
+                        double result = ServicioParqueo.this.getHour_Price() * num;
+                        return "El total a pagar es de: " + result;
+                    }
+                
+                    if (vehiculo.getService().equalsIgnoreCase("por dia")) {
+                        double result = ServicioParqueo.this.getDay_Price() * num;
+                        return "El total a pagar es de: " + result;
+                    }
+
+        } catch (InputMismatchException e) {
+            System.out.println("Solamente puede digitar un numero no letras ni caracteres, intente de nuevo!");
+        }
+        return "";
+    }
+
 }
