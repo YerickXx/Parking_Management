c11d031b2d0f9133fed42016dcbdc674dd5f55fb
Yerick Abarca <yerickabarca@gmail.com>
2026-02-12 15:36:21 -0600

    Se intenta agregar una validacion pero falla y se comienza a desarrollar la opcion 2 (Faltan validaciones y correciones cruciales)

--- OneDrive/Documentos/AbarcaYerick_Proyecto1_IC2026/AbarcaYerick_Proyecto1_IC2026/src/Objetos/GestorParqueo.java
+++ OneDrive/Documentos/AbarcaYerick_Proyecto1_IC2026/AbarcaYerick_Proyecto1_IC2026/src/Objetos/GestorParqueo.java
@@ -5,7 +5,6 @@
  * Primer Cuatrimestre 2026
  * Each line should be prefixed with  * 
  */
-
 // metodos  para el control de ocupacion del parqueo, administrar flujo vehicular
 package Objetos;
 
@@ -13,7 +12,7 @@ package Objetos;
  *
  * @author yeric
  */
-import java.util.InputMismatchException; 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.time.LocalDateTime; // paquete para trabajar de manera automatica la fecha y hora
 import java.time.format.DateTimeFormatter; // paquete para formatear la hora y dia
@@ -23,34 +22,38 @@ import java.util.LinkedList;
 public class GestorParqueo {
 
     Vehiculo vehiculo = new Vehiculo();
-    List<Vehiculo> autos = new LinkedList<>(); // almacenamiento de los medios de transporte
+    List<Vehiculo> vehiculos_en_parqueo = new LinkedList<>(); // almacenamiento de los medios de transporte
     ServicioParqueo services = new ServicioParqueo();
-
+    Scanner scanner = new Scanner(System.in);
+    boolean state = false;
+    boolean validacion_espacios = services.VerificacionEspacios(vehiculos_en_parqueo);
     public void Register_Vehicle() {
-        Scanner scanner = new Scanner(System.in);
-        boolean state = false; // estado para comprobar la placa del vehiculo
 
-        do { // ciclo de validacion de la placa
-            System.out.print("Ingrese la placa del vehiculo (formato: abc123): ");
-            String ops = scanner.next();
+        if (validacion_espacios) {
 
-                    for (int i = 0; i <= ops.length(); i++) // loop de validacion para un largo de cadena de 6 caracteres
-                    {
-                        if (i == 6) {
+            do { // ciclo de validacion de la placa
 
-                            if (ops.matches("^[a-zA-Z]{3}[0-9]+$") && !vehiculo.getPlate().equalsIgnoreCase(ops)) // Expresion regular para validar el valor AlphaNumerico de la placa
-                            {
-                                    
-                                    vehiculo.setPlate(ops.toUpperCase());
-                                    state = true;// estado cambia a verdadero para salir del loop de validacion
-                               
-                            } else {
-                                System.out.println("Se produjo un error en el ingreso de datos intente nuevamente!");
-                            }
+                System.out.print("Ingrese la placa del vehiculo (formato: abc123): ");
+
+                String ops = scanner.next();
+
+                for (int i = 0; i <= ops.length(); i++) // loop de validacion para un largo de cadena de 6 caracteres
+                {
+                    if (i == 6) {
+
+                        if (ops.matches("^[a-zA-Z]{3}[0-9]+$") && !vehiculo.getPlate().equalsIgnoreCase(ops)) // Expresion regular para validar el valor AlphaNumerico de la placa
+                        {
+                            vehiculo.setPlate(ops.toUpperCase());
+                            state = true;// estado cambia a verdadero para salir del loop de validacion
                         }
+                    } else {
+                        System.out.println("Se produjo un error en el ingreso de datos intente nuevamente!");
                     }
-        
-        } while (!state);
+                }
+            } while (!state);
+        } else {
+            System.out.println("El parqueo esta lleno! regrese mas tarde, gracias por su preferencia");
+        }
 
         String plate = vehiculo.getPlate();
         try {
@@ -77,50 +80,85 @@ public class GestorParqueo {
                 }
             }
             String type = vehiculo.getType();
-            
+
             System.out.println("Seleccione el tipo de servicio que desea adquirir: ");
             System.out.println("1. Servicio por hora");
             System.out.println("2. Servicio por dia");
             System.out.print("Seleccione su opcion: ");
             Integer service = scanner.nextInt();
-             switch (service) {
-                case 1 -> {
-                    vehiculo.setService("Por Hora");
-                    services.Calculo_Pago_Hora(type);
-                    break;
+            try {
+                switch (service) {
+                    case 1 -> {
+                        vehiculo.setService("Por Hora");
+                        services.Calculo_Pago_Hora(type);
+                        break;
+                    }
+                    case 2 -> {
+                        vehiculo.setService("Por dia");
+                        services.Calculo_Pago_dia(type);
+                        break;
+                    }
                 }
-                case 2 -> {
-                    vehiculo.setService("Por dia");
-                    services.Calculo_Pago_dia(type);
-                    break;
-                }
+            } catch (InputMismatchException error) {
+                System.out.println("Debe seleccionar solamente las opciones 1 o 2");
             }
-             String Service_type = vehiculo.getService();
+            String Service_type = vehiculo.getService();
+
             // Setteo automatico de fecha y hora de registro
             LocalDateTime Enter_Date = LocalDateTime.now();
             DateTimeFormatter Format_Date = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"); // formato de fecha y hora
             String formattedDate = Format_Date.format(Enter_Date);
             vehiculo.setEnter_Time(formattedDate);
             String enter = vehiculo.getEnter_Time();
-            
 
-            Vehiculo vehiculo2 = new Vehiculo(plate, type, enter, "Saliendo",Service_type);
-            autos.add(vehiculo2);
-            String d = services.Opciones_Tiempo(vehiculo2);
-           
+            Vehiculo vehiculo2 = new Vehiculo(plate, type, enter, "Saliendo", Service_type);
+            vehiculos_en_parqueo.add(vehiculo2);
+
+            String registro = services.Opciones_Tiempo(vehiculo2);
+
             System.out.println("=======================");
             System.out.println("          RECIBO         ");
             System.out.println("=======================");
-            System.out.println("Matricula registrada......."+ plate);
-            System.out.println("Tipo de vehiculo......."+ type);
-            System.out.println("Fecha y hora de ingreso al recinto......."+ enter);
-            System.out.println("Servicio solicitado......."+ Service_type);
-            System.out.println( d);
-            
-             
+            System.out.println("Matricula registrada......." + plate);
+            System.out.println("Tipo de vehiculo......." + type);
+            System.out.println("Fecha y hora de ingreso al recinto......." + enter);
+            System.out.println("Servicio solicitado......." + Service_type);
+            System.out.println(registro);
+
         } catch (InputMismatchException error) {
             System.out.println("Debe ingresar solamente los datos que le solicitan intente de nuevo!");
         }
     }
-   
+
+    public void Salida_Vehiculo() {
+        List<Vehiculo> vehiculos = GestorParqueo.this.vehiculos_en_parqueo;
+        System.out.print("Ingrese la matricula de su vehiculo: ");
+        String matricula = scanner.nextLine();
+        if (matricula.matches("^[a-zA-Z]{3}[0-9]{3}$"))
+        {
+            for(Vehiculo vehiculoh: vehiculos){
+                if(vehiculoh.getPlate().equalsIgnoreCase(matricula))
+                {
+                   vehiculos.removeIf(v -> v.getPlate().equalsIgnoreCase(matricula));
+                }}
+        }
+        DateTimeFormatter form_date = DateTimeFormatter.ofPattern("DD/MM/YYYY");
+        DateTimeFormatter form_hour = DateTimeFormatter.ofPattern("HH:MM");
+        System.out.print("Ingrese el dia en la que esta haciendo su salida del parqueo: ");
+        String input_date = scanner.nextLine();
+        System.out.print("Ingrese la hora en la que esta haciendo su salida del parqueo: ");
+        String input_hour = scanner.nextLine();
+        try {
+            LocalDateTime date = LocalDateTime.parse(input_date, form_date);
+            LocalDateTime hour = LocalDateTime.parse(input_hour, form_hour);
+            System.out.println("Fecha ingresada: " + date + " " + hour);
+        } catch (Exception e) {
+            System.out.println("Formato no valida por favor intente nuevamente!");
+        }
+    }
+
+    public void EspaciosParqueo() {
+        String f = services.EspaciosParqueo(vehiculos_en_parqueo);
+        System.out.println(f);
+    }
 }
